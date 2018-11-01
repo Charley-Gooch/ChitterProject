8ac53c76f85ca8a0601272b69c4dabf145046222
Yannis-Fabri <yannisk888@gmail.com>
2018-11-01 21:15:39 +0000

    connecting the database to the sign up page

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,10 @@
 require 'sinatra/base'
 require 'shotgun'
+require './app/details'
 
 class Chitter < Sinatra::Base
+  enable :method_override
+  set :method_override, true
 
   get '/' do
     erb :index
@@ -11,5 +14,10 @@ class Chitter < Sinatra::Base
     erb :login
   end
 
-run if app_file == $0
+  post '/' do
+    Details.create(name: params[:name], password: params[:password], email: params[:email])
+    redirect '/'
+  end
+
+  run if app_file == $0
 end
---
+++ app/details.rb
@@ -0,0 +1,18 @@
+require 'pg'
+
+class Details
+  attr_reader :id, :name, :password, :email
+
+  def initialize(id:, name:, passowrd:, email:)
+    @id = id
+    @name = name
+    @passowrd = password
+    @email = email
+  end
+
+  def self.create(name:, password:, email:)
+    connection = PG.connect(dbname: 'db_chitter')
+    result = connection.exec("INSERT INTO users (name, password, email) VALUES('#{name}', '#{password}', '#{email}') RETURNING id, name, password, email")
+    Details.new(id: result[0]['id'], name: result[0]['name'], password: result[0]['password'], email: result[0]['email'])
+  end
+end
